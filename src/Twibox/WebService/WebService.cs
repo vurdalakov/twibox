0bab16a8562179b4eaa50055e340333960a847e5
vurdalakov <vurdalakov@gmail.com>
2018-02-26 13:31:50 +0200

    Named pipe

--- src/Twibox/WebService/WebService.cs
+++ src/Twibox/WebService/WebService.cs
@@ -1,6 +1,9 @@
 ﻿namespace Vurdalakov
 {
     using System;
+    using System.IO;
+    using System.IO.Pipes;
+    using System.Threading.Tasks;
     using Nancy.Hosting.Self;
     using static DebugTracing;
 
@@ -14,11 +17,57 @@
 
             try
             {
+                var port = 61786;
+                var baseUrl = $"http://localhost:{port}/api/";
+
+                Task.Factory.StartNew(() =>
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            using (var server = new NamedPipeServerStream("DarkboxInfo"))
+                            {
+                                server.WaitForConnection();
+
+                                var reader = new StreamReader(server);
+                                var writer = new StreamWriter(server);
+
+                                while (true)
+                                {
+                                    var line = reader.ReadLine();
+                                    if (!String.IsNullOrEmpty(line))
+                                    {
+                                        switch (line.ToLower())
+                                        {
+                                            case "baseurl":
+                                                writer.WriteLine(baseUrl);
+                                                break;
+                                            case "popup":
+                                                mainForm.InvokeIfRequired(() => mainForm.BringToFront());
+                                                writer.WriteLine("OK");
+                                                break;
+                                            default:
+                                                writer.WriteLine("Unknown command");
+                                                break;
+                                        }
+                                        writer.Flush();
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Trace(ex, "Pipe error");
+                        }
+                    }
+                });
+
                 var hostConfiguration = new HostConfiguration();
                 hostConfiguration.RewriteLocalhost = false;
                 hostConfiguration.UnhandledExceptionCallback = ex => Trace(ex as Exception, "Unhandled web service exception");
 
-                this._nancyHost = new NancyHost(new Uri("http://localhost:61786/api/"), new WebServiceBootstrapper(mainForm), hostConfiguration);
+                this._nancyHost = new NancyHost(new Uri(baseUrl), new WebServiceBootstrapper(mainForm), hostConfiguration);
                 this._nancyHost.Start();
             }
             catch (Exception ex)
